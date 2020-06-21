b75c9de2ae49d4e210ba16378f82321294c47a20
Waquar <waquarahmd@gmail.com>
2020-06-21 23:09:37 +0530

    Initial setup and user input processing

---
+++ src/main/java/controller/ApplicationController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import service.ApplicationService;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class ApplicationController {
+    private static final String COMMA_SEPARATED_DELIMITER = ", ";
+    private static Scanner sc = new Scanner(System.in);
+    private ApplicationService applicationService = new ApplicationService();
+
+    public void takeUserInput() {
+        System.out.println("\nPlease enter your chess piece and it's current position separated by a space. Eg. King D5");
+        String userInputString = sc.nextLine();
+        System.out.println("You Entered : " + userInputString);
+        processUserInput(userInputString);
+    }
+
+    private void processUserInput(String userInputString) {
+        try {
+            List<String> possibleMoves = applicationService.getMovesFor(userInputString);
+            displayPossibleMovesToUser(possibleMoves);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        takeUserInput();
+    }
+
+    private void displayPossibleMovesToUser(List<String> possibleMoves) {
+        if (possibleMoves.isEmpty()) {
+            System.out.println("The player cannot move anywhere!");
+        } else {
+            System.out.print("Possible moves are: ");
+            StringJoiner sj = new StringJoiner(COMMA_SEPARATED_DELIMITER);
+            possibleMoves.forEach(sj::add);
+            System.out.println(sj);
+        }
+    }
+}
---
+++ src/main/java/service/ApplicationService.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ApplicationService {
+
+    public List<String> getMovesFor(String userInputString) {
+        return Collections.singletonList("D6, D7");
+    }
+
+}
