ea0cc869633348786c239186c1e275f73046cbfd
Waquar <waquarahmd@gmail.com>
2020-06-21 23:11:51 +0530

    Application Entry point

---
+++ src/main/java/ChessMovesSimApplication.java
@@ -0,0 +1,9 @@
+import controller.ApplicationController;
+
+public class ChessMovesSimApplication {
+
+    public static void main(String[] args) {
+        ApplicationController applicationController = new ApplicationController();
+        applicationController.takeUserInput();
+    }
+}
